c2206ae7397348e8b3f823592558cd69a132427c
seeo <aragonlotr5@gmail.com>
2019-05-16 03:01:26 +0800

    so I have now made it possible to simply submit via the enter keystroke

--- script.jsx
+++ script.jsx
@@ -17,6 +17,8 @@ class List extends React.Component {
   clickHandler = event =>{
     //when button is clicked, make the state of word back to null
     this.setState({ word: " " });
+    //and then as a precautionary measure, prevent page from trying to load the action url-> 'cause gonna change to submit button.
+    event.preventDefault();
     //and then also start pushing the 'text' into the list which currently begins as empty
 
     //first declare a new variable and assign to this.state.list; pushList is an array that will hold entered words
@@ -44,16 +46,22 @@ class List extends React.Component {
 
 
       console.log("rendering");
+      /*by wrapping the form tag around the input and button tags, and also setting the button type to submit,
+      I now have the ability to simply use the Enter/Return key on my keyboard to submit this form */
       return (
-        <div className="list">
-          <input onChange={(event) => {this.changeHandler(event)}}
-          value={this.state.word}
-          />
-          <button onClick = {(event)=>{this.clickHandler(event)}}
-            >add item
-          </button>
-          <ol> {items}</ol>
-        </div>
+
+            <div className = "list">
+              <form className="submit-form" onSubmit={(event) => { this.clickHandler(event) }}>
+                <input onChange={(event) => {this.changeHandler(event)}}
+                    value={this.state.word}
+                />
+                    <button type="submit">
+                        add item
+                    </button>
+                </form>
+                <ol> {items}</ol>
+            </div>
+
       );
   }
 }
