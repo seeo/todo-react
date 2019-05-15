0eb70cbdf7b44b71b49edb2b52da8c07018f1d83
seeo <aragonlotr5@gmail.com>
2019-05-16 02:30:39 +0800

    sets word back to null and also pushes the word into the list array

--- script.jsx
+++ script.jsx
@@ -10,12 +10,38 @@ class List extends React.Component {
   }
 
   changeHandler = event => {
-    this.setState({word:event.target.value});
-    console.log("change", event.target.value);
+    this.setState({word: event.target.value});
+    console.log("generating changed word...", event.target.value);
   }
+
+  clickHandler = event =>{
+    //when button is clicked, make the state of word back to null
+    this.setState({ word: " " });
+    //and then also start pushing the 'text' into the list which currently begins as empty
+
+    //first declare a new variable and assign to this.state.list; pushList is an array that will hold entered words
+    const pushList = this.state.list;
+    //also declare a new variable and assign to this.state.word; insertWord is the word that was typed and will be pushed into pushList array
+    const insertWord = this.state.word;
+    //let the pushing begin!
+    pushList.push(insertWord);
+
+    console.log("want to see what is inside list", this.state.list);
+    console.log("remove word from state but push...", event.target.value);
+  }
+
 
   render() {
       // render the list with a map() here
+    const list = this.state.list;
+    const items = list.map((item, index)=>{
+        return (
+            <li key = {index+item}>
+                {item}
+            </li>
+        )
+    });
+
 
       console.log("rendering");
       return (
@@ -23,7 +49,10 @@ class List extends React.Component {
           <input onChange={(event) => {this.changeHandler(event)}}
           value={this.state.word}
           />
-          <button>add item</button>
+          <button onClick = {(event)=>{this.clickHandler(event)}}
+            >add item
+          </button>
+          <ol> {items}</ol>
         </div>
       );
   }
